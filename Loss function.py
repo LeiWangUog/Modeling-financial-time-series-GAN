35c198b126377fa0b7bacef3f81db0e59dafa742
LeiWangUog <158491057+LeiWangUog@users.noreply.github.com>
2024-03-25 17:51:28 +0000

    Add files via upload

---
+++ Loss function.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import DataLoader, TensorDataset
+
+# 检查是否有可用的GPU，如果有，使用第一个GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
+
+# Adjusted initialization for generator and discriminator
+input_size = 100  # Latent space dimension
+output_channels = 1  # Assuming univariate time series
+sequence_length = 8400  # Target sequence length
+
+generator = Generator(input_size=100, output_channels=1, sequence_length=8400).to(device)
+discriminator = Discriminator(input_channels=1).to(device)
+
+# Define loss function and optimizers
+criterion = nn.BCELoss()  # Binary cross-entropy loss for real/fake classification
+optimizer_G = optim.Adam(generator.parameters(), lr=0.0003)  # Adam optimizer for generator
+optimizer_D = optim.Adam(discriminator.parameters(), lr=0.0001)  # Adam optimizer for discriminator
